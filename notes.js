618f4bd1d14982bd86767fdf30e1d19002df577f
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-08-27 15:20:58 -0400

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,71 @@
+//this is a comment
+//console.log("Hello World")
+
+//Declare variables
+// VAR:Will be use in the whole program
+//var student = "Cata"
+// LET:Will only be use in the scope of where you declare it
+//let student = "Cata"
+// CONST:Can NOT change
+//const student = "Cata"
+
+//Assigning a Variable
+//var a;
+//We assigned 2 to B
+//var b = 2;
+//a = 7;
+//b = a;
+//console.log(b)
+
+//Initialize varibles means to give them value
+
+//var sume = 10 + 9;
+//var resta = 98 - 23;
+//var multiplicacion = 9 * 7;
+//var dividir = 88 / 4;
+
+//Increment a number
+//var myVar = 3;
+//myVar++;
+//So now will be 4
+
+//Decrement a number
+//var myVar = 5;
+//myVar--;
+//So now will be 4
+
+//Decimal numbers are: 33.5 o 73.5 o 246.2
+
+//Remainders
+//var remainder;
+//remainder = 11 % 3;
+//The result is 2
+
+//var a = 23;
+//var b = 3;
+//a = a + 12;   esto lo puedo cambiar por      a += 12;
+//b = 9 + b;   esto lo puedo cambiar por      a += 9;
+//a = a - 12;   esto lo puedo cambiar por      a -= 12;
+//b = 9 - b;   esto lo puedo cambiar por      a -= 9;
+//a = a * 12;   esto lo puedo cambiar por      a *= 12;
+//b = 9 * b;   esto lo puedo cambiar por      a *= 9;
+//a = a * 12;   esto lo puedo cambiar por      a /= 12;
+//b = 9 * b;   esto lo puedo cambiar por      a /= 9;
+
+//Quotes inside a string
+//var myStr = "I am a \"double quoted\" string"
+
+//var anAdjective = "blue";
+//var ourStr = "The book is ";
+//ourStr += anAdjective;
+//console.log(ourStr)
+
+//this is to print the first letter
+//firstLetter = anAdjective[0];
+
+//How to change an string
+//var myStr = "Ji";
+//myStr = "Hi";
+
+//var firstName = "Andrea";
+//var lastLetter = firstName[firstName.length - 1];
